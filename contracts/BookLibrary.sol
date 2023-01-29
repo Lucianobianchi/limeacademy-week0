64a542fabe9be395dcf1d5df31872b2c83024c6c
Lucianobianchi <lucianogbianchi@gmail.com>
2023-01-28 18:54:19 -0800

    Final fixes. Added comments for submission.

--- contracts/BookLibrary.sol
+++ contracts/BookLibrary.sol
@@ -9,12 +9,12 @@ contract BookLibrary is Ownable {
     mapping (uint => mapping (address => bool)) booksBorrowed;
 
     // bookId -> int. Number of copies available to borrow for each book.
-    mapping (uint => uint) stockAvailable; 
+    mapping (uint => uint) public stockAvailable; 
 
     Book[] public books;
 
     // bookId -> list of users who previously borrowed it
-    mapping (uint => address[]) borrowHistory;
+    mapping (uint => address[]) public borrowHistory;
 
     struct Book {
         string name;
@@ -29,28 +29,47 @@ contract BookLibrary is Ownable {
     
     event BookReturned(uint id, address borrower, uint remaining);
     
+    /**
+     * @dev Borrow book from library
+     * @param _bookId id of the book to borrowed
+     */
     function borrowBook(uint _bookId) external {
         address borrower = msg.sender;
-        require(!booksBorrowed[_bookId][borrower]); // checks the user has not already borrowed the same book
-        require(stockAvailable[_bookId] > 0); // checks there are copies available
+        require(!booksBorrowed[_bookId][borrower], "User already borrowed this book"); 
+        require(stockAvailable[_bookId] > 0, "No book copies available"); 
 
         // can borrow
         booksBorrowed[_bookId][borrower] = true;
         borrowHistory[_bookId].push(borrower);
+        stockAvailable[_bookId]--;
 
         emit BookBorrowed(_bookId, borrower, stockAvailable[_bookId]);
     }
 
+    /**
+     * @dev Return book to library
+     * @param _bookId id of the book to return. Book has to be previously borrowed before returning.
+     */
     function returnBook(uint _bookId) external {
-        require(booksBorrowed[_bookId][msg.sender]);
+        require(booksBorrowed[_bookId][msg.sender], "User has not borrowed this book");
 
         // can return
         booksBorrowed[_bookId][msg.sender] = false;
+        stockAvailable[_bookId]++;
         
         emit BookReturned(_bookId, msg.sender, stockAvailable[_bookId]);
     }
 
-    function getAvailableBooks() external view returns(Book[] memory) {
+    struct BookAvailability {
+        uint bookId;
+        uint copies;
+    }
+
+    /**
+     * @dev Gets a list of all books with copies available for borrowing.
+     * @return availability_ array of BookAvailability, only contains books with one or more copies.
+     */
+    function getAvailableBooks() external view returns(BookAvailability[] memory availability_) {
         uint totalAvailableBooks = 0;
         for (uint i = 0; i < books.length; i++) {
             if (stockAvailable[i] > 0) {
@@ -58,32 +77,47 @@ contract BookLibrary is Ownable {
             }
         }
 
-        Book[] memory result = new Book[](totalAvailableBooks);
+        BookAvailability[] memory result = new BookAvailability[](totalAvailableBooks);
+        uint j = 0;
         for (uint i = 0; i < books.length; i++) {
             if (stockAvailable[i] > 0) {
-                result[i] = books[i];
+                result[j] = BookAvailability(i, stockAvailable[i]);
+                j++;
             }
         }
 
         return result;
     }
 
-    function getBorrowerHistory(uint _bookId) external view returns(address[] memory) {
-        return borrowHistory[_bookId];
-    }
-
+    /**
+     * @dev Add a book to the library.
+     * @param _name the book name
+     * @param _copies the initial copies of the book
+     */
     function addBook(string memory _name, uint _copies) external onlyOwner {
         books.push(Book(_name, true));
         uint bookId = books.length - 1;
 
         emit BookAdded(bookId);
 
-        setBookCopies(bookId, _copies);
+        setAvailableBookCopies(bookId, _copies);
     }
 
-    function setBookCopies(uint _bookId, uint _copies) public onlyOwner {
-        require(books[_bookId].inLibrary);
-        require(_copies > 0);
+    /**
+     * @dev Returns a list of all the borrowers for a book.
+     * @param _bookId id of the book
+     */
+    function getBorrowHistory(uint _bookId) external view returns(address[] memory) {
+        return borrowHistory[_bookId];
+    }
+
+    /**
+     * @dev Sets the amount of available copies for an existing book in the library.
+     * @param _bookId id of the book
+     * @param _copies new amount of copies. Will replace the existing number of available copies. 
+     */
+    function setAvailableBookCopies(uint _bookId, uint _copies) public onlyOwner {
+        require(books[_bookId].inLibrary, "Book not in library");
 
         uint previousCopies = stockAvailable[_bookId];
         stockAvailable[_bookId] = _copies;
